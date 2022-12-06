be43700fa4716dd4672c01fd6a415a844804c145
Luki128 <ktos.zsk@gmail.com>
2022-12-06 01:41:54 +0100

    Units+Todo List

--- LandOfBlood/LandOfBlood/LandOfBlood.cpp
+++ LandOfBlood/LandOfBlood/LandOfBlood.cpp
@@ -10,7 +10,41 @@
 #include "GameWorld.h"
 
 using namespace std;
-
+/*
+TODO list
+    GameLogic:
+        + Gerate Map
+        + Gerate Resources
+        + Player State Conotrol
+        + Units Movement
+        + Units Interraction
+        + Supoort selecting olny changed terrains
+        + Units send interraction provided(from base)
+        - Units send intteraction from terrain to another
+        - Proper fight handling(cannot atttack basa of antoher player if any toher terory exist)
+        - Proper room owner hdling when player dysconnects
+        - Calaculating units move cost
+        - Calcualating all units food cost by day
+        - Support structures on terrains
+        - Build/Upgrade strucutre(counitng cost) Inteeaction
+        - Support structures actions
+    Server:
+        -Support client hadling (join/register/left)
+        -Support variable parser(making pacckage)
+        -Support rpc(remote procdres control)
+        -Supoort contorl commnads
+    Client:
+        -Supports connectio/disconnetion form server 
+        -Supports rpc like srver
+        -display and upadte resources bar
+        -display and upadte map (units nad structs)
+        -display interface to conotrl game
+    Integation:
+        -registrtion protoocol(pass nick before join)
+        -sendig map upadte(worki ok on srver and client)
+        -sendig resource upadte(worki ok on srver and client)
+        -full control player by grafical intrafve via rpc 
+*/
 
 int main()
 {
